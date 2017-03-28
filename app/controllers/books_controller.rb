3c2dc0251bb2d4050b4e22e5a16b19dd6fda9268
Sebe Keller <sebethkeller@gmail.com>
2017-03-28 15:19:52 -0400

    Add validation and edit options for books

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,6 +4,11 @@ class BooksController < ApplicationController
 	end
 
 	def new
+		@book = Book.new
+	end
+
+	def edit
+		@book = Book.find(params[:id])
 	end
 
 	def show
@@ -12,8 +17,21 @@ class BooksController < ApplicationController
 
 	def create
 		@book = Book.new(book_params)
-		@book.save
-		redirect_to @book
+		if @book.save
+			redirect_to @book
+		else
+			render 'new'
+		end
+	end
+
+	def update
+		@book = Book.find(params[:id])
+
+		if @book.update(book_params)
+			redirect_to @book
+		else
+			render 'edit'
+		end
 	end
 
 	private
